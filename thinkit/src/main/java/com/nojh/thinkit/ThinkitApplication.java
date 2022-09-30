bd21bbdc5a97636905ddbef63f2b505c19ed5c4a
jgm0327 <gmjang6349@gmail.com>
2022-09-30 19:08:05 +0900

    #1 - 필요한 Dependency 들 추가
    spring web, jpa, MySQL Driver, Lombok 추가 추후에 필요한 게 있다면 그 때 그 때 추가할 예정.

---
+++ thinkit/src/main/java/com/nojh/thinkit/ThinkitApplication.java
@@ -0,0 +1,13 @@
+package com.nojh.thinkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkitApplication.class, args);
+    }
+
+}
