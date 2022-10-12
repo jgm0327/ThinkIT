bdbbba5c7dab97f0469de537b29a39d019306a06
ubuntu-jgm <gmjang6349@gmail.com>
2022-10-12 18:44:23 +0900

    앱에서 잘 받는지 테스트
    앱에서 잘 처리하는지 확인하기 위해서 GET /test 만듦

--- thinkit/src/main/java/com/nojh/thinkit/controller/BaseController.java
+++ thinkit/src/main/java/com/nojh/thinkit/controller/BaseController.java
@@ -1,23 +1,20 @@
 package com.nojh.thinkit.controller;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.nojh.thinkit.dto.ConceptDto;
+import com.nojh.thinkit.dto.KeywordDto;
+import com.nojh.thinkit.dto.ProblemDto;
 import com.nojh.thinkit.service.BaseService;
-import com.nojh.thinkit.vo.ConceptVO;
-import com.nojh.thinkit.vo.KeywordVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -27,24 +24,41 @@ public class BaseController {
     private final BaseService baseService;
 
     @GetMapping(value = "/keywords", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<KeywordVO> sendKeywords(@RequestParam("subject") String subject){
-        KeywordVO keywordVO = baseService.getKeywords(subject);
-        return new ResponseEntity<>(keywordVO, HttpStatus.OK);
+    public ResponseEntity<KeywordDto> sendKeywords(@RequestParam("subject") String subject) {
+        KeywordDto keywordDto = baseService.getKeywords(subject);
+        return new ResponseEntity<>(keywordDto, HttpStatus.OK);
     }
 
     @GetMapping("/concepts")
-    public ResponseEntity<ConceptVO> explanation(@RequestParam("keyword") String keyword){
-        ConceptVO conceptVO = baseService.getConceptContent(keyword);
-        return new ResponseEntity<>(conceptVO, HttpStatus.OK);
+    public ResponseEntity<ConceptDto> explanation(@RequestParam("keyword") String keyword) {
+        ConceptDto conceptDto = baseService.getConceptContent(keyword);
+        return new ResponseEntity<>(conceptDto, HttpStatus.OK);
     }
 
-    @GetMapping("/problems")
-    public void problems(){
-
-    }
+    /*@GetMapping("/problems")
+    public void problems(@RequestParam("subjects") List<String> subjects) {
+        ProblemDto problemDto = baseService.getProblems()
+    }*/
 
     @GetMapping("/interview-problems")
-    public void interview_problems(){
+    public void interview_problems() {
 
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<ProblemDto> test(@RequestParam("subjects") List<String> subjects) {
+        List<String> title = new ArrayList<>();
+        List<String> content = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            title.add("문제" + i);
+            String select = "";
+            for (int j = 1; j <= 3; j++) {
+                select += "보기" + i + "-" + j + ".";
+            }
+            select += "보기4";
+            content.add(select);
+        }
+        ProblemDto problemDto = new ProblemDto(title, content);
+        return new ResponseEntity<>(problemDto, HttpStatus.OK);
     }
 }
