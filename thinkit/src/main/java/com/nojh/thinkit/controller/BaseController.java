3b6f14a28399525af38b8510c0a7fb4e2bf6734d
ubuntu-jgm <gmjang6349@gmail.com>
2022-10-18 13:41:57 +0900

    #3 - 컨트롤러 완성
    클라이언트가 요청한 것을 서비스 로직이 처리하여 원하는 데이터를 만든 뒤 응답한다.

--- thinkit/src/main/java/com/nojh/thinkit/controller/BaseController.java
+++ thinkit/src/main/java/com/nojh/thinkit/controller/BaseController.java
@@ -1,19 +1,13 @@
 package com.nojh.thinkit.controller;
 
-import com.nojh.thinkit.dto.ConceptDto;
-import com.nojh.thinkit.dto.KeywordDto;
-import com.nojh.thinkit.dto.ProblemDto;
+import com.nojh.thinkit.dto.*;
 import com.nojh.thinkit.service.BaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -35,30 +29,21 @@ public class BaseController {
         return new ResponseEntity<>(conceptDto, HttpStatus.OK);
     }
 
-    /*@GetMapping("/problems")
-    public void problems(@RequestParam("subjects") List<String> subjects) {
-        ProblemDto problemDto = baseService.getProblems()
-    }*/
-
     @GetMapping("/interview-problems")
-    public void interview_problems() {
-
+    public ResponseEntity<InterviewProblemDto> interview_problems(@RequestParam("subjects") List<String> subjects) {
+        InterviewProblemDto interviewProblemDto = baseService.getInterviewProblems(subjects);
+        return new ResponseEntity<>(interviewProblemDto, HttpStatus.OK);
     }
 
-    @GetMapping("/test")
+    @GetMapping("/problems")
     public ResponseEntity<ProblemDto> test(@RequestParam("subjects") List<String> subjects) {
-        List<String> title = new ArrayList<>();
-        List<String> content = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            title.add("문제" + i);
-            String select = "";
-            for (int j = 1; j <= 3; j++) {
-                select += "보기" + i + "-" + j + ".";
-            }
-            select += "보기4";
-            content.add(select);
-        }
-        ProblemDto problemDto = new ProblemDto(title, content);
+        ProblemDto problemDto = baseService.getProblems(subjects);
         return new ResponseEntity<>(problemDto, HttpStatus.OK);
+    }
+
+    @PostMapping("/feedback")
+    public ResponseEntity<FeedbackDto> getFeedBack(@RequestParam("feedback") String content) {
+        FeedbackDto feedbackDto = baseService.getFeedback(content);
+        return new ResponseEntity<>(feedbackDto, HttpStatus.OK);
     }
 }
