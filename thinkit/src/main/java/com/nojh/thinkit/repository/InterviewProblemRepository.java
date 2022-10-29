50ada6e04a91d668abe92fcecaa0cdf1a232038f
ubuntu-jgm <gmjang6349@gmail.com>
2022-10-29 15:43:38 +0900

    Repository 수정 - 2
    원하는 쿼리문을 직접 짜서 구현함

--- thinkit/src/main/java/com/nojh/thinkit/repository/InterviewProblemRepository.java
+++ thinkit/src/main/java/com/nojh/thinkit/repository/InterviewProblemRepository.java
@@ -4,7 +4,12 @@ import com.nojh.thinkit.entity.InterviewProblem;
 import com.nojh.thinkit.entity.Problem;
 import com.nojh.thinkit.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface InterviewProblemRepository extends JpaRepository<InterviewProblem, Integer> {
-    public InterviewProblem findBySubjectAndId(Subject subject, Integer id);
+    @Query(value = "select * from interview_problems where subject_id = :subject_id order by rand() limit :cnt", nativeQuery = true)
+    public List<InterviewProblem> selectByRandom(@Param("subject_id") int subject_id, @Param("cnt") int cnt);
 }
--- thinkit/src/main/java/com/nojh/thinkit/repository/ProblemRepository.java
+++ thinkit/src/main/java/com/nojh/thinkit/repository/ProblemRepository.java
@@ -1,9 +1,13 @@
 package com.nojh.thinkit.repository;
 
 import com.nojh.thinkit.entity.Problem;
-import com.nojh.thinkit.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProblemRepository extends JpaRepository<Problem, Integer> {
-    public Problem findBySubjectAndId(Subject subject, Integer id);
+    @Query(value = "select * from problems where subject_id = :subject_id order by rand() limit :cnt", nativeQuery = true)
+    public List<Problem> selectByRandom(@Param("subject_id") int subject_id, @Param("cnt") int cnt);
 }
